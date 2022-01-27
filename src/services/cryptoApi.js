d359c0a8f3f076d700dbd4dc0b9bebd1bb013bf5
giacomo.satta <giacomo.satta@WINAPPW3CODDICP>
2022-01-27 14:52:22 +0100

    Homepage done with api call

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '929fc66e1dmshdc7c7acf7797329p1d759bjsn5d83e38f6636'
+
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) =>({url,headers:cryptoApiHeaders})
+ 
+export const cryptoApi = createApi({
+    reducerPath:'creyptoApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+    endpoints:(builder) => ({
+        getCryptos: builder.query({
+            query:() => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
