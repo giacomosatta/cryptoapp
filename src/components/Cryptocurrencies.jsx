0d721e7358559a102b233b9f0f4c67f78be11e62
giacomo.satta <giacomo.satta@WINAPPW3CODDICP>
2022-02-03 16:34:10 +0100

    Chart + o - funzionanti

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -8,10 +8,12 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 const Cryptocurrencies = ({simplified}) => {
   const count = simplified ? 10:100;
   const {data:cryptosList, isFetching} = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState();
   const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
+    setCryptos(cryptosList?.data?.coins);
+
     const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase())) 
 
     setCryptos(filteredData);
@@ -30,8 +32,8 @@ const Cryptocurrencies = ({simplified}) => {
       )}
           <Row gutter={[32,32]} className='crypto-card-container'>
               {cryptos?.map((currency) => (
-                  <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id} >
-                    <Link to={`/crypto/${currency.id}`}>
+                  <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid} >
+                    <Link to={`/crypto/${currency.uuid}`}>
                         <Card 
                             title={`${currency.rank}. ${currency.name}`}
                             extra={<img className='crypto-image' src={currency.iconUrl} />}
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -11,15 +11,23 @@ const baseUrl = 'https://coinranking1.p.rapidapi.com'
 const createRequest = (url) =>({url,headers:cryptoApiHeaders})
  
 export const cryptoApi = createApi({
-    reducerPath:'creyptoApi',
+    reducerPath:'cryptoApi',
     baseQuery:fetchBaseQuery({baseUrl}),
     endpoints:(builder) => ({
         getCryptos: builder.query({
             query:(count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptoDetails: builder.query({
+            query:(coinId) => createRequest(`/coin/${coinId}`)
+        }),
+        getCryptoHistory: builder.query({
+            query:({coinId, timePeriod}) => createRequest(`/coin/${coinId}/history?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=${timePeriod}`)
         })
     })
 });
 
 export const {
     useGetCryptosQuery,
+    useGetCryptoDetailsQuery,
+    useGetCryptoHistoryQuery
 } = cryptoApi;
